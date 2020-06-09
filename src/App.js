e47590ad3e245c180fe8f8494b89b47b563f40ce
Keating <mgsong@madup.com>
2020-06-09 17:33:07 +0900

    add `addMenu` api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React, { useState } from "react";
-import "./App.scss";
+import React, { useState } from 'react'
+import './App.scss'
+
+const API_URL = 'https://howaboutthis-api/api/add-menu'
 
 function App() {
-  const [title, setTitle] = useState("");
-  const [category, setCategory] = useState("");
-  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [url, setUrl] = useState('')
+  function addMenu() {
+    console.log('API_URL', API_URL)
+  }
   return (
     <div className="App">
       <h2>식당 추가</h2>
       <div className="item">
         <label>식당이름: </label>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <input value={title} onChange={e => setTitle(e.target.value)} />
       </div>
       <div className="item">
         <label>분류: </label>
-        <input value={category} onChange={(e) => setCategory(e.target.value)} />
+        <input value={category} onChange={e => setCategory(e.target.value)} />
       </div>
       <div className="item">
         <label>URL: </label>
-        <input value={url} onChange={(e) => setUrl(e.target.value)} />
+        <input value={url} onChange={e => setUrl(e.target.value)} />
       </div>
       <div className="item">
-        <button>추가</button>
+        <button onClick={addMenu}>추가</button>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

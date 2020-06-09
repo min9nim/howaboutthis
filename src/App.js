fde568b792a07588ab33e84aa3e0a9db8be9d74b
Keating <mgsong@madup.com>
2020-06-09 17:22:58 +0900

    set reformat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>식당 추가</h2>
+      <div>
+        <label>식당이름: </label>
+        <input />
+      </div>
+      <div>
+        <label>분류: </label>
+        <input />
+      </div>
+      <div>
+        <label>URL: </label>
+        <input />
+      </div>
     </div>
   );
 }

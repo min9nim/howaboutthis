3f2573a387de8d849a82bdb958277a806890c53f
Keating <mgsong@madup.com>
2020-06-09 17:30:13 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [url, setUrl] = useState("");
   return (
     <div className="App">
       <h2>식당 추가</h2>
-      <div>
+      <div className="item">
         <label>식당이름: </label>
-        <input />
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
       </div>
-      <div>
+      <div className="item">
         <label>분류: </label>
-        <input />
+        <input value={category} onChange={(e) => setCategory(e.target.value)} />
       </div>
-      <div>
+      <div className="item">
         <label>URL: </label>
-        <input />
+        <input value={url} onChange={(e) => setUrl(e.target.value)} />
+      </div>
+      <div className="item">
+        <button>추가</button>
       </div>
     </div>
   );

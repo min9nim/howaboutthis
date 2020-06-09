92fd95a98348777752750456fb78d480949b18d2
Keating <mgsong@madup.com>
2020-06-09 21:19:48 +0900

    load list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import req from './utils/req'
 import './App.scss'
 
@@ -6,9 +6,16 @@ function App() {
   const [title, setTitle] = useState('')
   const [category, setCategory] = useState('')
   const [url, setUrl] = useState('')
+  const [list, setList] = useState([])
+  useEffect(() => {
+    req('get-menus').then(res => {
+      setList(res.result)
+    })
+  })
   function addMenu() {
     req('add-menu', { title, category, url })
   }
+
   return (
     <div className="App">
       <h2>식당 추가</h2>
@@ -29,6 +36,11 @@ function App() {
       </div>
       <hr />
       <h2>식당 목록</h2>
+      <ul>
+        {list.map(({ _id, title, category, url }) => (
+          <li key={_id}>{title}</li>
+        ))}
+      </ul>
     </div>
   )
 }

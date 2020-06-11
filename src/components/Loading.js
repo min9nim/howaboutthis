aa6c9419bea0b0f18daf2d91e21b8b7a5f6b111a
Keating <mgsong@madup.com>
2020-06-11 09:10:51 +0900

    Loading to center

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import './Loading.scss'
 
 export default () => (
-  <div style={{ textAlign: 'center' }}>
+  <div className="Loading">
     <img src="/loading.gif" alt="loading" />
   </div>
 )

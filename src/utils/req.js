e84e95b523fc6d6b636dcfc4262f276ca0a6bcb3
min9nim <min9nim@gmail.com>
2020-06-15 09:16:18 +0900

    [refactor] new http

--- src/utils/req.js
+++ src/utils/req.js
@@ -1,41 +1,43 @@
+let $loading
+export function initLoading(loading) {
+  $loading = loading
+}
+
+export async function http(url, data, option) {
+  try {
+    $loading(true)
+    const res = await fetch(
+      url,
+      Object.assign(
+        {
+          method: 'POST', // *GET, POST, PUT, DELETE, etc.
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        },
+        option,
+      ),
+    )
+    if (!res.ok) {
+      window.$logger.warn('res is not ok')
+    }
+    $loading(false)
+    return res
+  } catch (e) {
+    $loading(false)
+    alert(e.message)
+    throw e
+  }
+}
+
 export default function req(path, data) {
-  return fetch(process.env.REACT_APP_API_URL + '/api/' + path, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  return http(process.env.REACT_APP_API_URL + '/api/' + path, data, {
     mode: 'cors', // no-cors, cors, *same-origin
-    // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json',
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    // redirect: 'follow', // manual, *follow, error
-    // referrer: 'no-referrer', // no-referrer, *client
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
   }).then(res => res.json())
 }
 
 export function webscrap(url) {
   console.log('process.env.REACT_APP_WEBSCRAP_URL', process.env.REACT_APP_WEBSCRAP_URL)
-  // return fetch(process.env.REACT_APP_WEBSCRAP_URL, {
-  return fetch(process.env.REACT_APP_WEBSCRAP_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ url }),
-  }).then(res => res.json())
-}
-
-export function messageToSlack(text) {
-  return fetch(process.env.REACT_APP_SLACK_URL, {
-    method: 'POST',
-    // mode: 'cors', // no-cors, cors, *same-origin
-    // headers: {
-    //   'Content-Type': 'application/json',
-    // },
-    body: JSON.stringify({
-      text,
-      // channel: process.env.REACT_APP_SLACK_CHANNEL
-    }),
-  }).then(res => res.text())
+  return http(process.env.REACT_APP_WEBSCRAP_URL, { url }).then(res => res.json())
 }

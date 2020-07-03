cf065036b7b199bf5d7f74d8d166b899af47ef19
keating529 <mgsong@madup.com>
2020-07-03 12:00:55 +0900

    댓글목록 기본값 설정

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './CommentForm.scss'
 import CommentItem from './CommentItem'
 
-export default function CommentList({ list, setList }) {
+export default function CommentList({ list = [], setList }) {
   return (
     <ul>
       {list.map((item, idx) => {

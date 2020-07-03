06d9ca08ca599e5f3f37dd775ac34ed4584c7b89
keating529 <mgsong@madup.com>
2020-07-03 09:27:24 +0900

    add padding

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -4,7 +4,7 @@ import './CommentForm.scss'
 export default function CommentItem({ item }) {
   const { author, content } = item
   return (
-    <li style={{ color: '#888' }}>
+    <li style={{ color: '#888', padding: '5px 0' }}>
       {author}) {content}
     </li>
   )

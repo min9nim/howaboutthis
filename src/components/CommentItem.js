c5d5222543f916c56b5f4c0e693b7b4d1673b7c6
keating529 <mgsong@madup.com>
2020-07-03 09:33:45 +0900

    add delete btn

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import './CommentForm.scss'
+import './CommentItem.scss'
 
 export default function CommentItem({ item }) {
   const { author, content } = item
   return (
-    <li style={{ color: '#888', padding: '5px 0' }}>
-      {author}) {content}
+    <li className="comment-item">
+      <div className="author">{author})</div>{' '}
+      <div className="content">{content}</div>{' '}
+      <div className="delete-btn">x️</div>
     </li>
   )
 }

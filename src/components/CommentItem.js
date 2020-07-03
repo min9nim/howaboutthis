2b5750991c5d856217798fdd9d84aad749c9144b
keating529 <mgsong@madup.com>
2020-07-03 10:07:41 +0900

    댓글 개행 처리

--- src/components/CommentItem.js
+++ src/components/CommentItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './CommentItem.scss'
 import req from '../utils/req'
 import { complement, propEq } from 'ramda'
+import { nl2br } from 'mingutils/dist'
 
 export default function CommentItem({ item, setList }) {
   const { _id, author, content } = item
@@ -15,8 +16,11 @@ export default function CommentItem({ item, setList }) {
   }
   return (
     <li className="comment-item">
-      <div className="author">{author})</div>
-      <div className="content">{content}</div>
+      <div className="author">{author || '익명'})</div>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: nl2br(content) }}
+      />
       <div className="delete-btn" onClick={handleDelete}>
         x️
       </div>
